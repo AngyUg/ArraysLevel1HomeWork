7d0782d43e90488fce2d763b33d5f6ef6de0f290
AngyUg <proofmyself@gmail.com>
2018-09-13 16:11:01 +0300

    task3

--- src/ArrayPositiveFinder.java
+++ src/ArrayPositiveFinder.java
@@ -1,2 +1,22 @@
 public class ArrayPositiveFinder {
-}
+    public int findFirstPositive(int array[]) {
+        int value = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                value = array[i];
+                break;
+            }
+        }
+        return value;
+    }
+    public int lastFirstPositive(int array[]) {
+        int value = 0;
+        for (int i = array.length - 1; i > 0; i--) {
+            if (array[i] > 0) {
+                value = array[i];
+                break;
+            }
+        }
+        return value;
+
+    }}
--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,18 @@ public class Main {
         ArrayFiller arrayFiller = new ArrayFiller();
         System.out.println("Array with filler: ");
         arrayFiller.fill(5);
+
+        System.out.println(" ");
+        ArrayPositiveFinder arrayPositiveFinder=new ArrayPositiveFinder();
+        int arrayS []={-1,2,6,4,-1};
+        int readValue;
+        int readValue2;
+        readValue=arrayPositiveFinder.findFirstPositive(arrayS);
+        System.out.println(" ");
+        System.out.println("First positive value in arrey is:="+readValue);
+        readValue2=arrayPositiveFinder.lastFirstPositive(arrayS);
+        System.out.println("Last positive value in arrey is:="+readValue2);
+
     }
+
 }
